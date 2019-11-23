527e775f684fb67a9848c2dc54bd6a8310713201
poojithabogadi <poojithabogadi96@gmail.com>
2019-11-23 17:01:24 +0530

    poojitha this is my first programme

---
+++ src/org/bank/AxisBank.java
@@ -0,0 +1,28 @@
+package org.bank;
+
+public class AxisBank extends BankInfo {
+	@Override
+	public void saving(int id) {
+		// TODO Auto-generated method stub
+		System.out.println("Emplyee savings is:"+ id);
+	}
+	
+@Override
+public void fixed(String name) {
+	// TODO Auto-generated method stub
+	System.out.println("employee has fixed deposit name:"+ name);
+}
+@Override
+public void Deposit(long number) {
+	// TODO Auto-generated method stub
+	System.out.println("employee has good package deposit:"+ number);
+}
+public static void main(String[] args) {
+	AxisBank a = new AxisBank();
+	a.saving(96);
+	a.fixed("poojitha");
+	a.Deposit(9652);
+}
+	
+
+}
---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,20 @@
+package org.bank;
+
+public class BankInfo {
+	public void saving(int id) {
+		
+		System.out.println("Emplyee savings is:"+ id);
+
+	}
+	public void fixed(String name) {
+		
+		System.out.println("employee has fixed deposit name:"+ name);
+
+	}
+	public void Deposit(long number) {
+		
+		System.out.println("employee has good package deposit:"+ number);
+
+	}
+
+}
